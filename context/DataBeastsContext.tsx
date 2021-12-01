4c01843b1a25173a1dca83af4d042244c17c70e5
Justin Singh <justinsingh2015@gmail.com>
2021-11-30 22:21:54 -0800

    Implement temporary fix to server side build error

--- context/DataBeastsContext.tsx
+++ context/DataBeastsContext.tsx
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react'
-import { TezosToolkit, Wallet } from "@taquito/taquito"
+import { TezosToolkit } from "@taquito/taquito"
 import { BeaconWallet } from "@taquito/beacon-wallet";
-import { Network, NetworkType, TezosOperationType } from "@airgap/beacon-sdk";
+import { Network, NetworkType } from "@airgap/beacon-sdk";
 
 // Type for Provider Component props
 type ProviderProps = {
@@ -30,54 +30,69 @@ export const useDataBeastsContext = () => {
 }
 
 const Tezos = new TezosToolkit("https://mainnet-tezos.giganode.io");
-if (typeof window !== 'undefined') {
-  const wallet = new BeaconWallet({
-    name: "DataBeasts",
-    preferredNetwork: NetworkType.MAINNET,
-  });
-  Tezos.setWalletProvider(wallet);
-}
-
 
 export const DataBeastsProvider = ({ children }: ProviderProps) => {
-  const [userAddress, setUserAddress] = useState<string | undefined>(undefined);
+  /* 
+  Problem - Creating a BeaconWallet object attempts to access the window's localStorage. 
+            However, window does not exist server side! It only exits client side.
+ 
+  Temporary Fix - Check if window exists to avoid server side build error. 
+                       
+  Potential Solutions - 1) Dynamic importing of BeaconWallet with SSR = false.
+                    I was advised to extract the error inducing logic to the component level and dynamically import it with SSR = false.
+                    https://nextjs.org/docs/advanced-features/dynamic-import
 
-  useEffect(() => {
-    initState();
-  }, []);
+                    2) Move client side code to useEffect
+  */
+  if (typeof window !== 'undefined') {
+    const [userAddress, setUserAddress] = useState<string | undefined>(undefined);
 
-  const initState = async () => {
-    const activeAccount = await wallet.client.getActiveAccount();
+    const wallet = new BeaconWallet({
+      name: "DataBeasts",
+      preferredNetwork: NetworkType.MAINNET,
+    });
+    Tezos.setWalletProvider(wallet);
 
-    if (activeAccount !== undefined) {
-      let address = await wallet.getPKH();
-      setUserAddress(address);
+    useEffect(() => {
+      initState();
+    }, []);
+
+    const initState = async () => {
+      const activeAccount = await wallet.client.getActiveAccount();
+
+      if (activeAccount !== undefined) {
+        let address = await wallet.getPKH();
+        setUserAddress(address);
+      }
     }
-  }
 
-  const syncWallet = async () => {
-    const network: Network = { type: NetworkType.MAINNET };
-    const activeAccount = await wallet.client.getActiveAccount();
+    const syncWallet = async () => {
+      const network: Network = { type: NetworkType.MAINNET };
+      const activeAccount = await wallet.client.getActiveAccount();
 
-    // Request wallet connection if no active account found
-    if (activeAccount === undefined) {
-      console.log("Requesting wallet connection");
-      await wallet.requestPermissions({ network });
-      let address = await wallet.getPKH();
-      setUserAddress(address);
-      console.log("New connection: ", address);
+      // Request wallet connection if no active account found
+      if (activeAccount === undefined) {
+        console.log("Requesting wallet connection");
+        await wallet.requestPermissions({ network });
+        let address = await wallet.getPKH();
+        setUserAddress(address);
+        console.log("New connection: ", address);
+      }
     }
-  }
 
-  const desyncWallet = async () => {
-    await wallet.client.clearActiveAccount()
-    console.log("Wallet disconnected");
-    setUserAddress(undefined);
-  }
+    const desyncWallet = async () => {
+      await wallet.client.clearActiveAccount()
+      console.log("Wallet disconnected");
+      setUserAddress(undefined);
+    }
 
-  return (
-    <DataBeastsContext.Provider value={{ userAddress, syncWallet, desyncWallet }} >
-      {children}
-    </DataBeastsContext.Provider>
-  )
+    return (
+      <DataBeastsContext.Provider value={{ userAddress, syncWallet, desyncWallet }} >
+        {children}
+      </DataBeastsContext.Provider>
+    )
+  }
+  else {
+    return <div></div>
+  }
 }
