04be317db4a3be7d14239f0a6bc5776be21e8f1f
Justin Singh <justinsingh2015@gmail.com>
2021-12-05 14:42:12 -0800

    Simplify ImageCard conditional rendering

--- components/ImageCard.tsx
+++ components/ImageCard.tsx
@@ -6,28 +6,27 @@ interface ImageCardProps {
   imageSrc: string
   imageAlt: string
   caption: string
-  href?: string | undefined
-  onClick?: any | undefined
-  ref: any | undefined
+  href?: string   // href is optional in the case of ImageCard being a child of a Link component with passHref enabled
+                  // In the above case, href would not be an explicit prop passed down to ImageCard
 }
 
-const ImageCard = React.forwardRef(({ imageSrc, imageAlt, caption, href=undefined, onClick=undefined, ref=undefined }: ImageCardProps) => {
-  if (!onClick) {
+const ImageCard = React.forwardRef(({ imageSrc, imageAlt, caption, href }: ImageCardProps, ref: any) => {
+  // If there is no ref being passed down to this component
+  if (!ref) {
     return (
-      <VStack as="a" href={href} target="_blank" bgColor="rgba(255, 255, 255, 0.85)" borderRadius={10} boxShadow={'md'}>
+      <VStack as="a" href={href} ref={ref} target="_blank" bgColor="rgba(255, 255, 255, 0.85)" borderRadius={10} boxShadow={'md'}>
         <Image src={imageSrc} alt={imageAlt} />
         <Text fontWeight="bold">{caption}</Text>
       </VStack>
     );
   }
 
+  // If there is a ref being passed down to this component (Such as from a Link component with passHref enabled)
   return (
-    <a href={href} onClick={onClick} ref={ref}>
-      <VStack cursor="pointer" bgColor="rgba(255, 255, 255, 0.85)" borderRadius={10} boxShadow={'md'}>
+      <VStack as="a" href={href} ref={ref} bgColor="rgba(255, 255, 255, 0.85)" borderRadius={10} boxShadow={'md'}>
         <Image src={imageSrc} alt={imageAlt} />
         <Text fontWeight="bold">{caption}</Text>
       </VStack>
-    </a>
   );
 });
 
