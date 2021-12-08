e3cb0997d83a16e98afd0c4d8f654c49dc75d7d9
Justin Singh <justinsingh2015@gmail.com>
2021-12-08 01:50:15 -0800

    Add Head component to collection/address page

--- pages/collection/[address].tsx
+++ pages/collection/[address].tsx
@@ -1,6 +1,7 @@
 import { useRouter } from "next/dist/client/router"
 import type { NextPage } from 'next'
 import Collection from "../../components/Collection"
+import Head from "next/dist/shared/lib/head"
 
 const CollectionPage: NextPage = () => {
   const router = useRouter();
@@ -8,6 +9,11 @@ const CollectionPage: NextPage = () => {
 
   return (
     <div>
+      <Head>
+        <title>DataBeasts</title>
+        <meta name="description" content="DataBeast Collection" />
+      </Head>
+
       {router.isReady && (<Collection address={address} />)}
     </div>
   )
