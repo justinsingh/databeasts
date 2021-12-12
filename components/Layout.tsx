7e58d74bdb77a146dc903db30b09f3c6715e9d0c
Justin Singh <justinsingh2015@gmail.com>
2021-12-12 01:36:55 -0800

    Implement conditional image usage for sync button

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 import Head from 'next/head'
 import faviconImage from '../public/databeasts_favicon.png'
 import { Container, Box, Image } from '@chakra-ui/react'
-import backgroundImage from '../public/webbackground.png'
+import backgroundImage from '../public/webbackground-2.png'
 import syncButtonImage from '../public/sync_button.png'
+import unsyncButtonImage from '../public/sync_button.png'
 import { useDataBeastsContext } from '../context/DataBeastsContext'
 
 type LayoutProps = {
@@ -21,7 +22,12 @@ const Layout = ({ children }: LayoutProps) => {
         <link rel="icon" href={faviconImage.src} />
       </Head>
       <Box cursor="pointer" pos="absolute" top={5} right={5}>
-        <Image userSelect="none" onClick={typeof userAddress === 'undefined' ? syncWallet : desyncWallet} width={[61, 90, 110]} height={[27, 41, 50]} src={syncButtonImage.src} />
+        <Image 
+          src={typeof userAddress === 'undefined' ? syncButtonImage.src : unsyncButtonImage.src}
+          onClick={typeof userAddress === 'undefined' ? syncWallet : desyncWallet} 
+          width={[61, 90, 110]} height={[27, 41, 50]} 
+          userSelect="none" 
+        />
       </Box>
       {children}
     </Container>
