ac9442d7424fcb6170cba9d030499cfddf7d6a0c
Justin Singh <justinsingh2015@gmail.com>
2021-12-15 23:57:11 -0800

    Add title image to collection route

--- pages/collection/[address].tsx
+++ pages/collection/[address].tsx
@@ -1,15 +1,23 @@
 import { useRouter } from "next/dist/client/router"
 import type { NextPage } from 'next'
 import Collection from "../../components/Collection"
+import { Box, VStack, Image } from "@chakra-ui/react"
+import titleImage from '/public/databeasts_website_gif.gif'
 
 const CollectionPage: NextPage = () => {
   const router = useRouter();
   const { address } = router.query;
 
   return (
-    <div>
-      {router.isReady && (<Collection address={address} />)}
-    </div>
+    <VStack>
+      <Box maxW="850px">
+        <Image width={[300, 600, 850]} height={[150, 300, 425]} src={titleImage.src} alt="DataBeasts Title Image" />
+      </Box>
+
+      <Box>
+        {router.isReady && (<Collection address={address} />)}
+      </Box>
+    </VStack>
   )
 }
 
