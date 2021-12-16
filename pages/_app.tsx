3a39c423551b980fdb94329ff98f03a64d2f16b4
Justin Singh <justinsingh2015@gmail.com>
2021-12-15 23:58:50 -0800

    Dynamically import DataBeastsProvider component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,11 @@
 import '../styles/globals.css'
 import dynamic from 'next/dynamic'
 import type { AppProps } from 'next/app'
-import { DataBeastsProvider } from '../context/DataBeastsContext'
+//import DataBeastsProvider from '../context/DataBeastsContext'
+const DataBeastsProvider = dynamic(
+  () => import ('../context/DataBeastsContext'),
+  { ssr: false }
+)
 import { ChakraProvider } from '@chakra-ui/react'
 const Layout = dynamic(
   () => import('../components/Layout'),
