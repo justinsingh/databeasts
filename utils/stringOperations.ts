bacfb30fa500cc531afae29615eb260a613f3a89
Justin Singh <justinsingh2015@gmail.com>
2021-12-15 23:56:31 -0800

    Add string operation utility function

--- utils/stringOperations.ts
+++ utils/stringOperations.ts
@@ -2,3 +2,7 @@ export const getBeastNameFromTitle = (tokenTitle: string) => {
   let colonIndex = tokenTitle.indexOf(':');
   return tokenTitle.substring(colonIndex+2, tokenTitle.length);
 }
+
+export const getHashFromIpfsURI = (displayURI: string) => {
+  return displayURI.split("//")[1];
+}
