116dac723156baa78d6bbca9f5ed5baba635577d
Justin Singh <justinsingh2015@gmail.com>
2021-12-15 23:59:53 -0800

    Remove unnecessary client-side conditional

--- context/DataBeastsContext.tsx
+++ context/DataBeastsContext.tsx
@@ -32,67 +32,52 @@ export const useDataBeastsContext = () => {
 const Tezos = new TezosToolkit("https://mainnet-tezos.giganode.io");
 
 export const DataBeastsProvider = ({ children }: ProviderProps) => {
-  /* 
-  Problem - Creating a BeaconWallet object attempts to access the window's localStorage. 
-            However, window does not exist server side! It only exits client side.
- 
-  Temporary Fix - Check if window exists to avoid server side build error. 
-                       
-  Potential Solutions - 1) Dynamic importing of BeaconWallet with SSR = false.
-                    I was advised to extract the error inducing logic to the component level and dynamically import it with SSR = false.
-                    https://nextjs.org/docs/advanced-features/dynamic-import
+  const [userAddress, setUserAddress] = useState<string | undefined>(undefined);
 
-                    2) Move client side code to useEffect
-  */
-  if (typeof window !== 'undefined') {
-    const [userAddress, setUserAddress] = useState<string | undefined>(undefined);
+  const wallet = new BeaconWallet({
+    name: "DataBeasts",
+    preferredNetwork: NetworkType.MAINNET,
+  });
+  Tezos.setWalletProvider(wallet);
 
-    const wallet = new BeaconWallet({
-      name: "DataBeasts",
-      preferredNetwork: NetworkType.MAINNET,
-    });
-    Tezos.setWalletProvider(wallet);
+  useEffect(() => {
+    initState();
+  }, []);
 
-    useEffect(() => {
-      initState();
-    }, []);
+  const initState = async () => {
+    const activeAccount = await wallet.client.getActiveAccount();
 
-    const initState = async () => {
-      const activeAccount = await wallet.client.getActiveAccount();
-
-      if (activeAccount !== undefined) {
-        let address = await wallet.getPKH();
-        setUserAddress(address);
-      }
+    if (activeAccount !== undefined) {
+      let address = await wallet.getPKH();
+      setUserAddress(address);
     }
-
-    const syncWallet = async () => {
-      const network: Network = { type: NetworkType.MAINNET };
-      const activeAccount = await wallet.client.getActiveAccount();
-
-      // Request wallet connection if no active account found
-      if (activeAccount === undefined) {
-        console.log("Requesting wallet connection");
-        await wallet.requestPermissions({ network });
-        let address = await wallet.getPKH();
-        setUserAddress(address);
-        console.log("New connection: ", address);
-      }
-    }
-
-    const desyncWallet = async () => {
-      await wallet.client.clearActiveAccount()
-      console.log("Wallet disconnected");
-      setUserAddress(undefined);
-    }
-
-    return (
-      <DataBeastsContext.Provider value={{ userAddress, syncWallet, desyncWallet }} >
-        {children}
-      </DataBeastsContext.Provider>
-    )
   }
-  else {
-    return <div></div>
+
+  const syncWallet = async () => {
+    const network: Network = { type: NetworkType.MAINNET };
+    const activeAccount = await wallet.client.getActiveAccount();
+
+    // Request wallet connection if no active account found
+    if (activeAccount === undefined) {
+      console.log("Requesting wallet connection");
+      await wallet.requestPermissions({ network });
+      let address = await wallet.getPKH();
+      setUserAddress(address);
+      console.log("New connection: ", address);
+    }
   }
+
+  const desyncWallet = async () => {
+    await wallet.client.clearActiveAccount()
+    console.log("Wallet disconnected");
+    setUserAddress(undefined);
+  }
+
+  return (
+    <DataBeastsContext.Provider value={{ userAddress, syncWallet, desyncWallet }} >
+      {children}
+    </DataBeastsContext.Provider>
+  )
 }
+
+export default DataBeastsProvider;
