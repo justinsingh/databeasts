7203601e0affc7db45d4f212fc29054cc1887dde
Justin Singh <justinsingh2015@gmail.com>
2021-12-22 20:41:35 -0800

    Store beast gifs locally and use IPFS if not found

--- components/CollectionEntry.tsx
+++ components/CollectionEntry.tsx
@@ -1,17 +1,22 @@
 import React from "react";
 import { CollectionEntryProps } from './Collection'
 import { getBeastNameFromTitle, getHashFromIpfsURI } from '../utils/stringOperations'
-import { Flex, VStack, Image, Box, HStack, Text } from "@chakra-ui/react";
-
+import { VStack, Image, Box, HStack, Text } from "@chakra-ui/react";
 
 const CollectionEntry = ({ quantity, token }: CollectionEntryProps) => {
-  const ipfsImageURL = "https://ipfs.io/ipfs/" + getHashFromIpfsURI(token.display_uri);
-  //const ipfsImageURL = "https://cloudflare-ipfs.com/ipfs/" + getHashFromIpfsURI(token.display_uri);
+  var entryImageSrc: string;
+
+  // Source the image from /public/beast_gifs. Use ipfs.io if not found.
+  try {
+    entryImageSrc = require('../public/beast_gifs/' + token.id + '.gif').default.src;
+  } catch (error: unknown) {
+    entryImageSrc = "https://ipfs.io/ipfs/" + getHashFromIpfsURI(token.display_uri);
+  }
 
   return (
     <Box m={2} paddingBottom={2} bgColor="rgba(255, 255, 255)" borderRadius={10} boxShadow={'md'}>
       <VStack>
-        <Image borderTopRadius={10} width={[150, 300, 400]} height={[150, 300, 400]} src={ipfsImageURL} />
+        <Image borderTopRadius={10} width={[150, 300, 400]} height={[150, 300, 400]} src={entryImageSrc} />
         <HStack fontWeight="bold" paddingTop={2} paddingBottom={2}>
           <Text>{getBeastNameFromTitle(token.title)}</Text>
           <Text>{"x" + quantity}</Text>
