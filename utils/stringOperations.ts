a27f65fd510d340e42c0a1c4c5aa85ef18c81b43
Justin Singh <justinsingh2015@gmail.com>
2021-12-24 19:18:31 -0800

    Implement utility to shorten a Tezos address

--- utils/stringOperations.ts
+++ utils/stringOperations.ts
@@ -6,3 +6,7 @@ export const getBeastNameFromTitle = (tokenTitle: string) => {
 export const getHashFromIpfsURI = (displayURI: string) => {
   return displayURI.split("//")[1];
 }
+
+export const shortenAddress = (address: string) => {
+  return address.slice(0, 5) + "..." + address.slice(address.length-5);
+}
