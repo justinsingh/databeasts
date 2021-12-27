2c52593c1f27d253f079361cbb7efe945c4a7130
Justin Singh <justinsingh2015@gmail.com>
2021-12-26 16:44:24 -0800

    Styling improvement

--- components/CollectionInfo.tsx
+++ components/CollectionInfo.tsx
@@ -8,20 +8,18 @@ type CollectionInfoProps = {
   distinctBeasts: number
 }
 
-const CollectionInfo = ({ address, totalBeasts, distinctBeasts}: CollectionInfoProps) => {
+const CollectionInfo = ({ address, totalBeasts, distinctBeasts }: CollectionInfoProps) => {
   return (
     <Flex paddingBottom={[1, 2]} marginBottom={3} paddingLeft={[5, 50]} paddingRight={[5, 50]} bgColor="rgba(255, 255, 255)" borderRadius={10} boxShadow={'md'}>
-      <Center>
-        <VStack wordBreak={"break-word"} fontWeight="bold" paddingTop={2} paddingBottom={2}>
-          <Text fontSize={[16, 32]}>{shortenAddress(address)} Collection</Text>
-          <HStack fontSize={[12, 24]} divider={<StackDivider borderColor='gray' />}>
-            <Text marginRight={2}>{totalBeasts} Total</Text>
-            <Text marginLeft={2}>{distinctBeasts} Distinct</Text>
-          </HStack>
-        </VStack>
-      </Center>
+      <VStack wordBreak={"break-word"} fontWeight="bold" paddingTop={2} paddingBottom={2}>
+        <Text fontSize={[16, 32]}>{shortenAddress(address)} Collection</Text>
+        <HStack spacing={5} fontSize={[12, 24]}>
+          <Text>{totalBeasts} Total</Text>
+          <Text>{distinctBeasts} Distinct</Text>
+        </HStack>
+      </VStack>
     </Flex>
-  ) 
+  )
 }
 
 export default CollectionInfo
