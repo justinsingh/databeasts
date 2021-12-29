fc464a8a370d8b25f6e3cc8b73c4596e23c2003a
Justin Singh <justinsingh2015@gmail.com>
2021-12-29 00:05:34 -0800

    Implement utility to get beast number

--- utils/stringOperations.ts
+++ utils/stringOperations.ts
@@ -10,3 +10,18 @@ export const getHashFromIpfsURI = (displayURI: string) => {
 export const shortenAddress = (address: string) => {
   return address.slice(0, 5) + "..." + address.slice(address.length-5);
 }
+
+export const getBeastNumberFromTitle = (title: string) => {
+  /*
+  Title format = "DataBeasts#X: [beastName]" 
+  The '#' will always be at index 10.
+  But the ':' will be at a position dependent on the number of digits X has.
+  */
+
+  // Get index of ':' character in title
+  let colonIndex = title.indexOf(':');
+   
+  // Return the string that is between '#' and ':', which will contain the beast number.
+  console.log(title);
+  return title.slice(11, colonIndex);
+}
