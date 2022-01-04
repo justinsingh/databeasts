5b1ff7c74a358bc51a269a3ea30aa14f92dabe37
Justin Singh <justinsingh2015@gmail.com>
2022-01-03 22:49:58 -0800

    Add padding to bottom of Collection page

--- pages/collection/[address].tsx
+++ pages/collection/[address].tsx
@@ -8,7 +8,7 @@ const CollectionPage: NextPage = () => {
   const { address } = router.query;
 
   return (
-    <VStack spacing={0}>
+    <VStack paddingBottom="25vh" spacing={0}>
       {router.isReady && (<Collection address={address} />)}
     </VStack>
   )
