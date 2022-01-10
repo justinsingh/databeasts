2b36947a02a3ea0cb04074febf927580f6f9416a
Justin Singh <justinsingh2015@gmail.com>
2022-01-10 01:27:53 -0800

    Add meta tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,6 +10,9 @@ import twitterImage from '../public/twitter.gif'
 import viewImage from '../public/view.gif'
 import { VStack, Center, Wrap, WrapItem, Fade } from '@chakra-ui/react'
 import About from '../components/About'
+import Head from 'next/head'
+import metaTitleImage from '../public/databeasts_title_meta_image.png'
+import faviconImage from '../public/databeasts_favicon.png'
 
 const Home: NextPage = () => {
   const { userAddress } = useDataBeastsContext();
@@ -20,43 +23,65 @@ const Home: NextPage = () => {
   }
 
   return (
-    <Center>
-      <VStack width={["80%", "100%", "100%"]} pb={0} spacing={[5, 75]}>
-        <Wrap maxW={"75vw"} spacing={[4, 6, 7]} justify="center">
-          <WrapItem onClick={() => toggleShowAbout()}>
-            <ImageCard imageSrc={aboutImage.src} imageAlt="About DataBeasts" caption="About" />
-          </WrapItem>
+    <>
+      <Head>
+        <title>DataBeasts</title>
+        <meta name="description" content="Interact With Your DataBeasts" />
+        <link rel="icon" href={faviconImage.src} />
+        <title>DataBeasts</title>
+        <meta name="title" content="DataBeasts" />
+        <meta name="description" content="View and share your DataBeasts collection." />
 
-          <WrapItem>
-            {typeof userAddress === 'undefined' ?
-              <ImageCard imageSrc={viewImage.src} imageAlt="View Your DataBeasts" caption="View" isSyncAlert />
-              :
-              <Link href={"/collection/" + userAddress} passHref>
-                <ImageCard imageSrc={viewImage.src} imageAlt="View Your DataBeasts" caption="View" />
-              </Link>
-            }
-          </WrapItem>
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.databeasts.xyz/" />
+        <meta property="og:title" content="DataBeasts" />
+        <meta property="og:description" content="View and share your DataBeasts collection." />
+        <meta property="og:image" content="https://github.com/justinsingh/databeasts/blob/main/public/databeasts_title_meta_image.png?raw=true" />
 
-          <WrapItem>
-            <ImageCard imageSrc={shopImage.src} imageAlt="Buy DataBeasts" caption="Shop" href="https://objkt.com/profile/databeasts/created" />
-          </WrapItem>
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://www.databeasts.xyz/" />
+        <meta property="twitter:title" content="DataBeasts" />
+        <meta property="twitter:description" content="View and share your DataBeasts collection." />
+        <meta property="twitter:image" content="https://github.com/justinsingh/databeasts/blob/main/public/databeasts_title_meta_image.png?raw=true" />
+      </Head>
+      <Center>
+        <VStack width={["80%", "100%", "100%"]} pb={0} spacing={[5, 75]}>
+          <Wrap maxW={"75vw"} spacing={[4, 6, 7]} justify="center">
+            <WrapItem onClick={() => toggleShowAbout()}>
+              <ImageCard imageSrc={aboutImage.src} imageAlt="About DataBeasts" caption="About" />
+            </WrapItem>
 
-          <WrapItem>
-            <ImageCard imageSrc={discordImage.src} imageAlt="DataBeasts Discord" caption="Discord" href="http://discord.gg/Rn7YQqPhsm" />
-          </WrapItem>
+            <WrapItem>
+              {typeof userAddress === 'undefined' ?
+                <ImageCard imageSrc={viewImage.src} imageAlt="View Your DataBeasts" caption="View" isSyncAlert />
+                :
+                <Link href={"/collection/" + userAddress} passHref>
+                  <ImageCard imageSrc={viewImage.src} imageAlt="View Your DataBeasts" caption="View" />
+                </Link>
+              }
+            </WrapItem>
 
-          <WrapItem>
-            <ImageCard imageSrc={twitterImage.src} imageAlt="DataBeasts Twitter" caption="Twitter" href="https://mobile.twitter.com/databeasts" />
-          </WrapItem>
-        </Wrap>
+            <WrapItem>
+              <ImageCard imageSrc={shopImage.src} imageAlt="Buy DataBeasts" caption="Shop" href="https://objkt.com/profile/databeasts/created" />
+            </WrapItem>
 
-        {showAbout && (
-          <Fade in={showAbout}>
-            <About />
-          </Fade>
-        )}
-      </VStack>
-    </Center>
+            <WrapItem>
+              <ImageCard imageSrc={discordImage.src} imageAlt="DataBeasts Discord" caption="Discord" href="http://discord.gg/Rn7YQqPhsm" />
+            </WrapItem>
+
+            <WrapItem>
+              <ImageCard imageSrc={twitterImage.src} imageAlt="DataBeasts Twitter" caption="Twitter" href="https://mobile.twitter.com/databeasts" />
+            </WrapItem>
+          </Wrap>
+
+          {showAbout && (
+            <Fade in={showAbout}>
+              <About />
+            </Fade>
+          )}
+        </VStack>
+      </Center>
+    </>
   )
 }
 
