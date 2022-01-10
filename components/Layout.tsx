57a079df5ebd1715c09a29211fa8b4eccd339265
Justin Singh <justinsingh2015@gmail.com>
2022-01-10 00:51:09 -0800

    Update meta tags

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,44 +1,42 @@
 import React from 'react';
 import Head from 'next/head'
-import Link from 'next/link'
+//import Link from 'next/link'
 import faviconImage from '../public/databeasts_favicon.png'
 import { Container, Box, Image, HStack, Center } from '@chakra-ui/react'
-import backgroundImage from '../public/webbackground-2.png'
-import syncButtonImage from '../public/sync_button.png'
-import unsyncButtonImage from '../public/unsync_button.png'
-import { useDataBeastsContext } from '../context/DataBeastsContext'
-import titleImage from '/public/databeasts_website_gif.gif'
+//import backgroundImage from '../public/webbackground-2.png'
+//import syncButtonImage from '../public/sync_button.png'
+//import unsyncButtonImage from '../public/unsync_button.png'
+//import { useDataBeastsContext } from '../context/DataBeastsContext'
+//import titleImage from '/public/databeasts_website_gif.gif'
+import metaTitleImage from '../public/databeasts_title_meta_image.png'
 
 type LayoutProps = {
   children: React.ReactNode
 }
 
 const Layout = ({ children }: LayoutProps) => {
-  const { userAddress, syncWallet, desyncWallet } = useDataBeastsContext();
-
   return (
-    <Container maxW="100vw" minH="100vh" bgImage={backgroundImage.src} bgAttachment="fixed" bgSize="cover" bgPosition="center" bgRepeat="no-repeat">
+    <Container maxW="100vw" minH="100vh">
       <Head>
         <title>DataBeasts</title>
         <meta name="description" content="Interact With Your DataBeasts" />
         <link rel="icon" href={faviconImage.src} />
-      </Head>
-      <Center>
-        <Box mt={10} mb={10} maxW="850px">
-          <Link href="/">
-            <Image cursor="pointer" width={[300, 600, 850]} height={[150, 300, 425]} src={titleImage.src} alt="DataBeasts Title Image" />
-          </Link>
-        </Box>
-      </Center>
+        <title>DataBeasts</title>
+        <meta name="title" content="DataBeasts" />
+        <meta name="description" content="View and share your DataBeasts collection." />
 
-      <Box cursor="pointer" pos="absolute" top={5} right={5}>
-        <Image
-          src={typeof userAddress === 'undefined' ? syncButtonImage.src : unsyncButtonImage.src}
-          onClick={typeof userAddress === 'undefined' ? syncWallet : desyncWallet}
-          width={[61, 90, 110]} height={[27, 41, 50]}
-          userSelect="none"
-        />
-      </Box>
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.databeasts.xyz/" />
+        <meta property="og:title" content="DataBeasts" />
+        <meta property="og:description" content="View and share your DataBeasts collection." />
+        <meta property="og:image" content={metaTitleImage.src} />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://www.databeasts.xyz/" />
+        <meta property="twitter:title" content="DataBeasts" />
+        <meta property="twitter:description" content="View and share your DataBeasts collection." />
+        <meta property="twitter:image" content={metaTitleImage.src} />
+      </Head>
       {children}
     </Container>
   )
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import '../styles/globals.css'
-//import dynamic from 'next/dynamic'
+import dynamic from 'next/dynamic'
 import type { AppProps } from 'next/app'
 //import DataBeastsProvider from '../context/DataBeastsContext'
 /*
@@ -9,17 +9,17 @@ const DataBeastsProvider = dynamic(
 )
 */
 import { ChakraProvider } from '@chakra-ui/react'
-/*
 const Layout = dynamic(
   () => import('../components/Layout'),
   { ssr: false }
 )
-*/
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </ChakraProvider>
   )
 }
