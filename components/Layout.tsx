b234d7ec7c737b0b1b817d26868a85addecfadd7
Justin Singh <justinsingh2015@gmail.com>
2022-01-10 00:55:58 -0800

    Meta tag changes

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -17,26 +17,6 @@ type LayoutProps = {
 const Layout = ({ children }: LayoutProps) => {
   return (
     <Container maxW="100vw" minH="100vh">
-      <Head>
-        <title>DataBeasts</title>
-        <meta name="description" content="Interact With Your DataBeasts" />
-        <link rel="icon" href={faviconImage.src} />
-        <title>DataBeasts</title>
-        <meta name="title" content="DataBeasts" />
-        <meta name="description" content="View and share your DataBeasts collection." />
-
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://www.databeasts.xyz/" />
-        <meta property="og:title" content="DataBeasts" />
-        <meta property="og:description" content="View and share your DataBeasts collection." />
-        <meta property="og:image" content={metaTitleImage.src} />
-
-        <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content="https://www.databeasts.xyz/" />
-        <meta property="twitter:title" content="DataBeasts" />
-        <meta property="twitter:description" content="View and share your DataBeasts collection." />
-        <meta property="twitter:image" content={metaTitleImage.src} />
-      </Head>
       {children}
     </Container>
   )
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,17 +9,42 @@ const DataBeastsProvider = dynamic(
 )
 */
 import { ChakraProvider } from '@chakra-ui/react'
+import faviconImage from '../public/databeasts_favicon.png'
+import metaTitleImage from '../public/databeasts_title_meta_image.png'
+import Head from 'next/head'
+/*
 const Layout = dynamic(
   () => import('../components/Layout'),
   { ssr: false }
 )
+*/
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
+
     <ChakraProvider>
-      <Layout>
+<Head>
+        <title>DataBeasts</title>
+        <meta name="description" content="Interact With Your DataBeasts" />
+        <link rel="icon" href={faviconImage.src} />
+        <title>DataBeasts</title>
+        <meta name="title" content="DataBeasts" />
+        <meta name="description" content="View and share your DataBeasts collection." />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.databeasts.xyz/" />
+        <meta property="og:title" content="DataBeasts" />
+        <meta property="og:description" content="View and share your DataBeasts collection." />
+        <meta property="og:image" content={metaTitleImage.src} />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://www.databeasts.xyz/" />
+        <meta property="twitter:title" content="DataBeasts" />
+        <meta property="twitter:description" content="View and share your DataBeasts collection." />
+        <meta property="twitter:image" content={metaTitleImage.src} />
+      </Head>
+ 
         <Component {...pageProps} />
-      </Layout>
     </ChakraProvider>
   )
 }
