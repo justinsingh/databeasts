435a1eed9132ac987831797b1740fc446f3ae4c2
Justin Singh <justinsingh2015@gmail.com>
2022-01-18 18:17:18 -0800

    Add Tezos Domain to Context's Provider component

--- context/DataBeastsContext.tsx
+++ context/DataBeastsContext.tsx
@@ -4,6 +4,7 @@ import { BeaconWallet } from "@taquito/beacon-wallet";
 import { Network, NetworkType } from "@airgap/beacon-sdk";
 import { TaquitoTezosDomainsClient } from '@tezos-domains/taquito-client'
 import { Tzip16Module } from '@taquito/tzip16'
+import { fetchReverseRecord } from '../utils/tezosDomains'
 
 // Type for Provider Component props
 type ProviderProps = {
@@ -12,7 +13,8 @@ type ProviderProps = {
 
 // Type for DataBeasts Context
 type DataBeastsContextType = {
-  userAddress: string | undefined
+  userAddress: string | undefined,
+  userTezosDomain: string | undefined,
   syncWallet: () => void
   desyncWallet: () => void
   TezosDomains: TaquitoTezosDomainsClient
@@ -46,6 +48,7 @@ const TezosDomains = new TaquitoTezosDomainsClient({
 
 export const DataBeastsProvider = ({ children }: ProviderProps) => {
   const [userAddress, setUserAddress] = useState<string | undefined>(undefined);
+  const [userTezosDomain, setUserTezosDomain] = useState<string | undefined>(undefined);
 
   useEffect(() => {
     wallet = new BeaconWallet({
@@ -79,8 +82,19 @@ export const DataBeastsProvider = ({ children }: ProviderProps) => {
       console.log("Requesting wallet connection");
       await wallet.requestPermissions({ network });
       let address = await wallet.getPKH();
-      setUserAddress(address); // By changing state, we rerender the provider
+      setUserAddress(address); 
       console.log("New connection: ", address);
+
+      // Set Tezos Domain for user address if it exists
+      fetchReverseRecord({ address }).then(reverseRecord => {
+        let items = reverseRecord.items;
+
+        // Set domain name. Use undefined if not present
+        let domainName = items.length > 0 ? items[0].domain.name : undefined;
+
+        // Set tezosDomainName to domainName
+        setUserTezosDomain(domainName);
+      })
     }
   }
 
@@ -88,6 +102,7 @@ export const DataBeastsProvider = ({ children }: ProviderProps) => {
     await wallet.client.clearActiveAccount()
     console.log("Wallet disconnected");
     setUserAddress(undefined);
+    setUserTezosDomain(undefined);
   }
 
   const destroyClient = async () => {
@@ -96,7 +111,7 @@ export const DataBeastsProvider = ({ children }: ProviderProps) => {
   }
 
   return (
-    <DataBeastsContext.Provider value={{ userAddress, syncWallet, desyncWallet, TezosDomains, Tezos }} >
+    <DataBeastsContext.Provider value={{ userAddress, userTezosDomain, syncWallet, desyncWallet, TezosDomains, Tezos }} >
       {children}
     </DataBeastsContext.Provider>
   )
