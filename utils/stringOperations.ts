6ee93bacad6d96d34215cf4b5a869eb5cbc63ecc
Justin Singh <justinsingh2015@gmail.com>
2022-01-25 23:50:08 -0800

    Extract beast functions to beastOperations

--- utils/stringOperations.ts
+++ utils/stringOperations.ts
@@ -1,28 +1,9 @@
-export const getBeastNameFromTitle = (tokenTitle: string) => {
-  let colonIndex = tokenTitle.indexOf(':');
-  return tokenTitle.substring(colonIndex+2, tokenTitle.length);
-}
-
 export const getHashFromIpfsURI = (displayURI: string) => {
   return displayURI.split("//")[1];
 }
 
 export const shortenAddress = (address: string) => {
   return address.slice(0, 5) + "..." + address.slice(address.length-5);
-}
-
-export const getBeastNumberFromTitle = (title: string) => {
-  /*
-  Title format = "DataBeasts#X: [beastName]" 
-  The '#' will always be at index 10.
-  But the ':' will be at a position dependent on the number of digits X has.
-  */
-
-  // Get index of ':' character in title
-  let colonIndex = title.indexOf(':');
-   
-  // Return the string that is between '#' and ':', which will contain the beast number.
-  return title.slice(11, colonIndex);
 }
 
 export const isTezosDomainName = (address: string) => {
