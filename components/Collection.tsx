e4535654081ce22f8c08040e199a5e583f105d04
Justin Singh <justinsingh2015@gmail.com>
2022-01-28 00:56:46 -0800

    Remove collection sorting functions due to their new location

--- components/Collection.tsx
+++ components/Collection.tsx
@@ -7,7 +7,6 @@ import { getTezosAddressFromName, getTezosNameFromAddress } from "../utils/tezos
 import { useDataBeastsContext } from '../context/DataBeastsContext'
 import { fetchCollection } from '../utils/hicdex'
 import { fetchDomainRecord, fetchReverseRecord } from '../utils/tezosDomains'
-import { rare15EditionBeasts } from '../constants/beastData'
 
 type CollectionProps = {
   /*
@@ -16,40 +15,6 @@ type CollectionProps = {
   This causes the need for additional checks.
   */
   address: string | string[] | undefined
-}
-
-const sortByOldest = (a: CollectionEntryProps, b: CollectionEntryProps): number => {
-  return (a.token.timestamp > b.token.timestamp) ? 1 : -1
-}
-
-const sortByNewest = (a: CollectionEntryProps, b: CollectionEntryProps): number => {
-  return (b.token.timestamp > a.token.timestamp) ? 1 : -1
-}
-
-const sortByEditionCountDesc = (a: CollectionEntryProps, b: CollectionEntryProps): number => {
-  // Sort beasts with lower edition counts in front of beasts with higher edition counts
-  if (a.token.supply < b.token.supply) return -1;
-  if (a.token.supply > b.token.supply) return 1;
-
-  // Sort equal edition numbered beasts with purple color in front of blue beasts
-  if (rare15EditionBeasts.includes(a.token.id)) return -1;
-  if (rare15EditionBeasts.includes(b.token.id)) return 1;
-
-  // Sort equal edition numbered beasts by timestamp (most recent beasts first)
-  return (a.token.timestamp > b.token.timestamp) ? -1 : 1; 
-}
-
-const sortByEditionCountAsc = (a: CollectionEntryProps, b: CollectionEntryProps): number => {
-  // Sort beasts with higher edition counts in front of beasts with lower edition counts
-  if (a.token.supply < b.token.supply) return 1;
-  if (a.token.supply > b.token.supply) return -1;
-
-  // Sort equal edition numbered beasts with purple color behind blue beasts
-  if (rare15EditionBeasts.includes(a.token.id)) return 1;
-  if (rare15EditionBeasts.includes(b.token.id)) return -1;
-
-  // Sort equal edition numbered beasts by timestamp (most recent beasts first)
-  return (a.token.timestamp > b.token.timestamp) ? -1 : 1;
 }
 
 const Collection = ({ address }: CollectionProps) => {
