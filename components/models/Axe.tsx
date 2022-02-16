6059a0cc8e975be7a573d3b482d84f5ffd7fa663
Justin Singh <justinsingh2015@gmail.com>
2022-02-16 15:17:18 -0800

    Modify mesh

--- components/models/Axe.tsx
+++ components/models/Axe.tsx
@@ -5,13 +5,27 @@ import { useGLTF } from "@react-three/drei"
 export default function Model(props: any) {
   const group = useRef()
   const { nodes, materials } = useGLTF('/3D_models/minecraft_diamond_axe/diamondAxe.gltf')
-  console.log(nodes);
-  console.log(materials);
+
+  const meshes = () => {
+    return (
+      <>
+        <mesh geometry={nodes.mesh_0.geometry} material={materials["Material.001"]} />
+        <mesh geometry={nodes.mesh_1.geometry} material={materials["Material.002"]} />
+        <mesh geometry={nodes.mesh_2.geometry} material={materials["Material.003"]} />
+        <mesh geometry={nodes.mesh_3.geometry} material={materials["Material.004"]} />
+        <mesh geometry={nodes.mesh_4.geometry} material={materials["Material.005"]} />
+        <mesh geometry={nodes.mesh_5.geometry} material={materials["Material.006"]} />
+        <mesh geometry={nodes.mesh_6.geometry} material={materials["Material.007"]} />
+        <mesh geometry={nodes.mesh_7.geometry} material={materials["Material.008"]} />
+        <mesh geometry={nodes.mesh_8.geometry} material={materials["Material.009"]} />
+      </>
+    )
+  }
+
   return (
-    <group ref={group} dispose={null} {...props}>
+    <group ref={group} dispose={null} {...props} >
       <group rotation={[0, Math.PI / 1.8, -0.3]} scale={0.5}>
-        <mesh geometry={nodes.mesh_0.geometry} material={materials.material_2} />
-        <mesh geometry={nodes.mesh_1.geometry} material={materials.material_3} />
+
       </group>
     </group>
   )
