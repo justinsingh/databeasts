14825b8f0bbbbe06557dfd233c48400fc7c6fbef
Justin Singh <justinsingh2015@gmail.com>
2022-05-22 12:48:42 -0700

    Add beast data for ethereals with 5 editions

--- constants/beastData.tsx
+++ constants/beastData.tsx
@@ -2,4 +2,7 @@
 export const common10EditionBeasts = [472830, 472857];
 
 // OBJKT IDs of purple rare beasts with 15 editions
-export const rare15EditionBeasts = [572968, 649048, 692067]
+export const rare15EditionBeasts = [572968, 649048, 692067];
+
+// OBJKT IDs of green ethereal beasts with 5 editions
+export const ethereal5EditionBeasts = [737048];
